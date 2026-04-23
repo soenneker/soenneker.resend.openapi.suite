e8e78e07ce9b7938ef27e4b6a83502c7a09305b3
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 10:45:53 -0500

    tunit

--- test/Soenneker.Resend.OpenApi.Suite.Tests/ResendOpenApiSuiteTests.cs
+++ test/Soenneker.Resend.OpenApi.Suite.Tests/ResendOpenApiSuiteTests.cs
@@ -1,16 +1,15 @@
-﻿using Soenneker.Tests.FixturedUnit;
-using Xunit;
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Resend.OpenApi.Suite.Tests;
 
-[Collection("Collection")]
-public sealed class ResendOpenApiSuiteTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public sealed class ResendOpenApiSuiteTests : HostedUnitTest
 {
-    public ResendOpenApiSuiteTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public ResendOpenApiSuiteTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
